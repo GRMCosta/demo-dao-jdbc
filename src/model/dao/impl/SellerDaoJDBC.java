20293b36e8a21ea6ce36a7585b5e081430bdb080
Gian Costa <gian.costa@consulting-for.edenred.com>
2021-02-17 21:24:04 -0300

    SelleDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+  @Override
+  public void insert(Seller seller) {
+
+  }
+
+  @Override
+  public void update(Seller seller) {
+
+  }
+
+  @Override
+  public void deleteById(Integer id) {
+
+  }
+
+  @Override
+  public void findById(Integer id) {
+
+  }
+
+  @Override
+  public List<Seller> findAll() {
+    return null;
+  }
+}
