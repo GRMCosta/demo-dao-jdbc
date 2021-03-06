d689dde912005db055e39acf0a3a72bb7bd87834
Gian Costa <gian.costa@consulting-for.edenred.com>
2021-03-06 13:03:13 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,6 +88,19 @@ public class SellerDaoJDBC implements SellerDao {
   @Override
   public void deleteById(Integer id) {
 
+    PreparedStatement preparedStatement = null;
+    try {
+      preparedStatement = connection.prepareStatement("DELETE  FROM seller WHERE Id = ?");
+
+      preparedStatement.setInt(1, id);
+
+      preparedStatement.executeUpdate();
+    } catch (SQLException exception) {
+      throw new DbException(exception.getMessage());
+    } finally {
+      DB.closeStatement(preparedStatement);
+    }
+
   }
 
   @Override
